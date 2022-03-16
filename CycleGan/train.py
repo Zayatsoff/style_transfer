5d69675921c0d558854bd649588bdb25a5572a8d
Zayatsoff <rozin.lior@gmail.com>
2022-03-16 00:52:52 -0400

    Update train.py

---
+++ CycleGan/train.py
@@ -0,0 +1,23 @@
+import torch
+from dataset_load import HorseZebraDataset
+import sys
+from utils import save_checkpoint, load_checkpoint
+from torch.utils.data import DataLoader
+import torch.nn as nn
+import torch.optim as optim
+import config
+from torchvision.utils import save_image
+from disc_model import Disc
+from gen_model import Gen
+
+
+def train_fn():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()

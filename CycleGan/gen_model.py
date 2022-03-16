400f8d9a5c95347bd265ac24dca3e389b13af39b
Zayatsoff <rozin.lior@gmail.com>
2022-03-15 23:28:17 -0400

    Update gen_model.py

---
+++ CycleGan/gen_model.py
@@ -0,0 +1,63 @@
+import torch
+import torch.nn as nn
+
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, down=True, use_act=True, **kwards):
+        super(ConvBlock, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, padding_mode="reflect", **kwards)
+            if down
+            else nn.ConvTranspose2d(in_channels, out_channels, **kwards),
+            nn.InstanceNorm2d(out_channels),
+            nn.ReLU(inplace=True) if use_act else nn.Identity(),
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, channels):
+        super(ResidualBlock, self).__init__()
+        self.block = nn.Sequential(
+            ConvBlock(channels, channels, kernel_size=3, padding=1),
+            ConvBlock(channels, channels, use_act=False, kernel_size=3, padding=1),
+        )
+
+    def forward(self, x):
+        return x + self.block(x)
+
+
+class Gen(nn.Module):
+    def __init__(self, img_channels, num_features=64, num_residual=9):
+        super(Gen, self).__init__()
+        self.initial = nn.Sequential(
+            nn.Conv2d(
+                img_channels,
+                num_features,
+                kernel_size=7,
+                stride=1,
+                padding=3,
+                padding_mode="reflect",
+            ),
+            nn.ReLU(inplace=True),
+        )
+        self.down_blocks = nn.Sequential(
+            [
+                ConvBlock(
+                    num_features, num_features * 2, kernel_size=3, stride=2, padding=1
+                ),
+                ConvBlock(
+                    num_features * 2,
+                    num_features * 4,
+                    kernel_size=3,
+                    stride=2,
+                    padding=1,
+                ),
+            ]
+        )
+
+        self.redisual_blocks = nn.Sequential(
+            *[ResidualBlock(num_features * 4) for _ in range(num_residual)]
+        )

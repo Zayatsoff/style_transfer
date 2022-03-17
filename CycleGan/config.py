dcc81ea9662113e4a7e4e72b78d576e5a856e969
Zayatsoff <rozin.lior@gmail.com>
2022-03-17 02:55:35 -0400

    Update config.py

--- CycleGan/config.py
+++ CycleGan/config.py
@@ -24,7 +24,7 @@ transforms = A.Compose(
         A.Resize(width=256, height=256),
         A.HorizontalFlip(p=0.5),
         A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], max_pixel_value=255),
-        ToTensorV2,
+        ToTensorV2(),
     ],
     additional_targets={"image0": "image"},
 )

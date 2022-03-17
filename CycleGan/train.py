5f1107b0b16e3095ab259499a9ce05ada82c38b5
Zayatsoff <rozin.lior@gmail.com>
2022-03-17 02:50:40 -0400

    Update train.py

--- CycleGan/train.py
+++ CycleGan/train.py
@@ -9,10 +9,68 @@ import config
 from torchvision.utils import save_image
 from disc_model import Disc
 from gen_model import Gen
+from tqdm import tqdm
 
 
-def train_fn():
-    pass
+def train_fn(
+    disc_H, disc_Z, gen_Z, gen_H, loader, opt_disc, l1, mse, d_scaler, g_scaler
+):
+    loop = tqdm(loader, leave=True)
+    for idx, (zebra, horse) in enumerate(loop):
+        zebra = zebra.to(config["DEVICE"])
+        horse = horse.to(config["DEVICE"])
+
+        # Train disc H and Z
+        with torch.cuda.amp.autocast():
+            fake_horse = gen_H(zebra)
+            D_H_real = disc_H(horse)
+            D_H_fake = disc_H(fake_horse.detach())
+            D_H_real_loss = mse(D_H_real, torch.ones_like(D_H_real))
+            D_H_fake_loss = mse(D_H_fake, torch.ones_like(D_H_fake))
+            D_H_loss = D_H_real_loss + D_H_fake_loss
+
+            fake_zebra = gen_Z(horse)
+            D_Z_real = disc_Z(zebra)
+            D_Z_fake = disc_Z(fake_zebra.detach())
+            D_Z_real_loss = mse(D_Z_real, torch.ones_like(D_Z_real))
+            D_Z_fake_loss = mse(D_Z_fake, torch.ones_like(D_Z_fake))
+            D_Z_loss = D_Z_real_loss + D_Z_fake_loss
+
+            # put together
+            D_loss = (D_H_loss + D_Z_loss) / 2
+
+        opt_disc.zero_grad()
+        d_scaler.scale(D_loss).backward()
+        d_scaler.step(opt_disc)
+        d_scaler.update()
+
+        # train gen H and Z
+        with torch.cuda.amp.autocast():
+            # adversarial loss for both gens
+            D_H_fake = disc_H(fake_horse)
+            D_Z_fake = disc_Z(fake_zebra)
+            loss_G_H = mse(D_H_fake, torch.ones_like(D_H_fake))
+            loss_G_Z = mse(D_Z_fake, torch.ones_like(D_Z_fake))
+
+            # cycle loss
+            cycle_zebra = gen_Z(fake_horse)
+            cycle_horse = gen_H(fake_zebra)
+            cycle_zebra_loss = l1(zebra, cycle_zebra)
+            cycle_horse_loss = l1(horse, cycle_horse)
+            # indentity loss
+            indentity_zebra = gen_Z(zebra)
+            indentity_horse = gen_H(horse)
+            identity_zebra_loss = l1(zebra, indentity_zebra)
+            identity_horse_loss = l1(horse, indentity_horse)
+            # add together
+            G_loss = (
+                loss_G_Z
+                + loss_G_H
+                + cycle_zebra_loss * config["LAMBDA_CYCLE"]
+                + cycle_horse_loss * config["LAMBDA_CYCLE"]
+                + identity_horse_loss * config["LAMBDA_IDENTITY"]
+                + identity_zebra_loss * config["LAMBDA_IDENTITY"]
+            )
 
 
 def main():

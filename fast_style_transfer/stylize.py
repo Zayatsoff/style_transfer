2485b8d46e390e0a21a949f642f081d40722712a
Zayatsoff <rozin.lior@gmail.com>
2022-03-27 00:57:21 -0400

    Bug fixes

--- fast_style_transfer/stylize.py
+++ fast_style_transfer/stylize.py
@@ -6,9 +6,6 @@ import torchvision.transforms as transforms
 import torchvision.models as models
 from torchvision.utils import save_image
 
-device = torch.device("cuda" if torch.cuda.is_available else "cpu")
-# layers 0-5-10-19-28
-model = models.vgg19(pretrained=True).features
 
 # Hyperparams
 config = {
@@ -17,9 +14,9 @@ config = {
     "alpha": 1,
     "beta": 0.01,
 }
-img = {"galilee": "./styles/galilee.jpg"}
-
-style = {"monalisa": "./styles/monalisa.jpg"}
+img = {"img0": "./styles/galilee.jpg"}
+style = {"style0": "./styles/monalisa.jpg"}
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 class VGG19(nn.Module):
@@ -41,7 +38,7 @@ class VGG19(nn.Module):
 
 def load_img(image_name):
     img = Image.open(image_name)
-    img_loader = loader(img).unsqueeze(0)
+    img = loader(img).unsqueeze(0)
     return img.to(device)
 
 
@@ -49,19 +46,30 @@ img_size = 356
 
 loader = transforms.Compose(
     [
-        transforms.Resize(img_size),
+        transforms.Resize((img_size, img_size)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ]
+)
+
+invTrans = transforms.Compose(
+    [
+        transforms.Normalize(
+            mean=[0.0, 0.0, 0.0], std=[1 / 0.229, 1 / 0.224, 1 / 0.225]
+        ),
+        transforms.Normalize(mean=[-0.485, -0.456, -0.406], std=[1.0, 1.0, 1.0]),
     ]
 )
 
 
 # freeze the weights
 model = VGG19().to(device).eval()
-og_img = load_img(img["galilee"])
-style_img = load_img(img["monalisa"])
-generated = og_img.clone().requires_grad_(True)
-# generated = torch.randn(load_image(image["galilee"]).shape, device=device, requires_grad=True)
+og_img = load_img(img["img0"])
+style_img = load_img(style["style0"])
+# generated = og_img.clone().requires_grad_(True)
+generated = torch.randn(
+    load_img(img["galilee"]).shape, device=device, requires_grad=True
+)
 
 optimizer = optim.Adam([generated], lr=config["lr"])
 for step in range(config["total_steps"]):
@@ -85,14 +93,15 @@ for step in range(config["total_steps"]):
         )
 
         A = style_feature.view(channel, height * width).mm(
-            style.view(channel, height * width).t()
+            style_feature.view(channel, height * width).t()
         )
         style_loss += torch.mean((G - A) ** 2)
-    total_loss = config["alpha"] * config["beta"] * style_loss
+    total_loss = config["alpha"] * og_loss + config["beta"] * style_loss
     optimizer.zero_grad()
     total_loss.backward()
     optimizer.step()
 
-    if step % 200 == 0:
+    if step % 1000 == 0:
         print("total loss : ", total_loss)
-        save_image(generated, "generated.png")
+        save_gen = invTrans(generated)
+        save_image(save_gen, f"generated_{step}.png")

451cb8b19e0d7073bbc2e8b049ee859824b8545c
Zayatsoff <rozin.lior@gmail.com>
2022-03-26 22:48:58 -0400

    Create implementation of neural styles

---
+++ fast_style_transfer/stylize.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from PIL import Image
+import torchvision.transforms as transforms
+import torchvision.models as models
+from torchvision.utils import save_image
+
+model = models.vgg19(pretrained=True).features
+print(model)
